38cace7dbf72ee387f438e33d0e52541578340aa
Kenneth D. Evensen <kevensen@redhat.com>
2018-02-18 13:51:03 -0500

    Adding gobin to path

--- fedora-profile.sh
+++ fedora-profile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
-export PATH=$PATH:$JAVA_HOME
 export GOPATH=/home/default/go
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
+export PATH=$PATH:$JAVA_HOME:$GOPATH/bin
--- rhel-profile.sh
+++ rhel-profile.sh
@@ -2,5 +2,5 @@
 export M2_HOME=/usr/bin/apache-maven-3.5.2
 export M2=$M2_HOME/bin
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
-export PATH=$PATH:$JAVA_HOME:$M2
 export GOPATH=/homde/default/go
+export PATH=$PATH:$JAVA_HOME:$M2:$GOPATH/bin
